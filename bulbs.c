a32730499466254372236a2123d962426dff9066
TomasNagy <tomi.nagy@outlook.com>
2023-05-16 17:27:12 +0200

    Add bulbs.c

---
+++ bulbs.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+	// Get text from user
+
+	// Convert text to binary and print out the result with bulbs
+}
+
+void print_bulbs(string text)
+{
+	
+}
